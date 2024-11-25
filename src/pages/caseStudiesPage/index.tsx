db4a809e340aff94884f93d569760d20f967d63e
Prospertech-github <ikechukwuprosper8@gmail.com>
2024-11-26 00:58:35 +0100

    FEAT: Completed the filtering logic for Case Studies

--- src/pages/caseStudiesPage/index.tsx
+++ src/pages/caseStudiesPage/index.tsx
@@ -15,11 +15,19 @@ const caseStudyButtons = [
 interface Case {
   title: string;
   description: string; 
-  imgUrl: string
+  imgUrl: string,
+  segment: string
 }
 
 const CaseStudiesPage = () => {
   const [selectedCaseStudy, setSelectedCaseStudy] = useState(caseStudyButtons[0]);
+
+  const pciCases = caseStudy.filter((item:Case) => item.segment === "PCI DSS")
+  const ismsCases = caseStudy.filter((item:Case) => item.segment === "ISO 27001 (ISMS)")
+  const bcmsCases = caseStudy.filter((item:Case) => item.segment === "ISO 22301 (BCMS)")
+  const qmsCases = caseStudy.filter((item:Case) => item.segment === "ISO 9001 (QMS)")
+
+  // const filteredCase = caseStudy.map(case => case.segment == selectedCaseStudy)
 
   return (
     <motion.section
@@ -69,9 +77,9 @@ const CaseStudiesPage = () => {
           })}
         </section>
 
-        <section className="px[110px] py-[120px] flex justify-center">
-          <div className="flex flex-wrap gap-x-[48px] gap-y-[120px] justify-center items-center w-full">
-            {caseStudy.map((study:Case, index:number) => {
+        <section className="px-[110px] py-[120px] flex justify-center">
+          <div className="flex flex-wrap gap-x-[48px] gap-y-[120px] justify-around items-center w-full">
+            {selectedCaseStudy === "ALL" && caseStudy.map((study:Case, index:number) => {
               return (
                 <div className="h-[504px] w-[350px] flex flex-col" key={index}>
                   <div className="h-[180px] w-full">
@@ -108,6 +116,160 @@ const CaseStudiesPage = () => {
                 </div>
               );
             })}
+
+
+            {selectedCaseStudy === "PCI DSS" && pciCases.map((study:Case, index:number) => {
+              return (
+                <div className="h-[504px] w-[350px] flex flex-col" key={index}>
+                  <div className="h-[180px] w-full">
+                    <img src={study.imgUrl} alt="Case Study 1" className="h-full"/>
+                  </div>
+                  <div className="flex flex-col gap-[18px]">
+                    <Text
+                    variant="h5"
+                      align="left"
+                      fontFamily="gambetta"
+                      color="af-green"
+                      customClassName="font-medium"
+                    >
+                      {study.title}
+                    </Text>
+
+                    <Text
+                    variant="h5"
+                      align="left"
+                      fontFamily="proxima-nova"
+                      color="af-dark-green"
+                      customClassName="font-normal"
+                    >
+                      {study.description}
+                    </Text>
+
+                    <Button
+                      buttonType="button"
+                      label="Learn More"
+                      variant="secondary"
+                      customClassName="w-fit"
+                    />
+                  </div>
+                </div>
+              );
+            })}
+
+            {selectedCaseStudy === "ISO 27001 (ISMS)" && ismsCases.map((study:Case, index:number) => {
+              return (
+                <div className="h-[504px] w-[350px] flex flex-col" key={index}>
+                  <div className="h-[180px] w-full">
+                    <img src={study.imgUrl} alt="Case Study 1" className="h-full"/>
+                  </div>
+                  <div className="flex flex-col gap-[18px]">
+                    <Text
+                    variant="h5"
+                      align="left"
+                      fontFamily="gambetta"
+                      color="af-green"
+                      customClassName="font-medium"
+                    >
+                      {study.title}
+                    </Text>
+
+                    <Text
+                    variant="h5"
+                      align="left"
+                      fontFamily="proxima-nova"
+                      color="af-dark-green"
+                      customClassName="font-normal"
+                    >
+                      {study.description}
+                    </Text>
+
+                    <Button
+                      buttonType="button"
+                      label="Learn More"
+                      variant="secondary"
+                      customClassName="w-fit"
+                    />
+                  </div>
+                </div>
+              );
+            })}
+
+            {selectedCaseStudy === "ISO 22301 (BCMS)" && bcmsCases.map((study:Case, index:number) => {
+              return (
+                <div className="h-[504px] w-[350px] flex flex-col" key={index}>
+                  <div className="h-[180px] w-full">
+                    <img src={study.imgUrl} alt="Case Study 1" className="h-full"/>
+                  </div>
+                  <div className="flex flex-col gap-[18px]">
+                    <Text
+                    variant="h5"
+                      align="left"
+                      fontFamily="gambetta"
+                      color="af-green"
+                      customClassName="font-medium"
+                    >
+                      {study.title}
+                    </Text>
+
+                    <Text
+                    variant="h5"
+                      align="left"
+                      fontFamily="proxima-nova"
+                      color="af-dark-green"
+                      customClassName="font-normal"
+                    >
+                      {study.description}
+                    </Text>
+
+                    <Button
+                      buttonType="button"
+                      label="Learn More"
+                      variant="secondary"
+                      customClassName="w-fit"
+                    />
+                  </div>
+                </div>
+              );
+            })}
+
+            {selectedCaseStudy === "ISO 9001 (QMS)" && qmsCases.map((study:Case, index:number) => {
+              return (
+                <div className="h-[504px] w-[350px] flex flex-col" key={index}>
+                  <div className="h-[180px] w-full">
+                    <img src={study.imgUrl} alt="Case Study 1" className="h-full"/>
+                  </div>
+                  <div className="flex flex-col gap-[18px]">
+                    <Text
+                    variant="h5"
+                      align="left"
+                      fontFamily="gambetta"
+                      color="af-green"
+                      customClassName="font-medium"
+                    >
+                      {study.title}
+                    </Text>
+
+                    <Text
+                    variant="h5"
+                      align="left"
+                      fontFamily="proxima-nova"
+                      color="af-dark-green"
+                      customClassName="font-normal"
+                    >
+                      {study.description}
+                    </Text>
+
+                    <Button
+                      buttonType="button"
+                      label="Learn More"
+                      variant="secondary"
+                      customClassName="w-fit"
+                    />
+                  </div>
+                </div>
+              );
+            })}
+
           </div>
         </section>
       </Outlet>

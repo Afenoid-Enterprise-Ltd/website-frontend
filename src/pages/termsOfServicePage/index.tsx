66787a6dc362bf3eeabf0de42a394acf8969b79a
Edozie Onyeanusi <phynnedo@gmail.com>
2024-12-24 22:09:34 +0100

    fix: added consultation modal to page under construction

--- src/pages/termsOfServicePage/index.tsx
+++ src/pages/termsOfServicePage/index.tsx
@@ -1,10 +1,58 @@
-
+import { useState } from "react";
+import {
+  PageUnderConstruction,
+  RequestConsultation,
+  ConsultationForm,
+} from "../../components";
+import { motion } from "framer-motion";
 
 const TermsOfServicePage = () => {
+
+  const [isConsultationOpen, setIsConsultationOpen] = useState<boolean>(false);
+  const [isContactOpen, setIsContactOpen] = useState<boolean>(false);
+
+  const openConsultationModal = () => {
+    setIsConsultationOpen(true);
+  };
+
+  const closeConsultationModal = () => {
+    setIsConsultationOpen(false);
+  };
+
+  const openContactModal = () => {
+    setIsContactOpen(true);
+  };
+
+  const closeContactModal = () => {
+    setIsContactOpen(false);
+  };
+
+  const redirectToContactForm = () => {
+    closeConsultationModal();
+    openContactModal();
+  };
+
   return (
-    <h1>
-      Terms of Service Page
-    </h1>
+    <motion.section
+    initial={{ opacity: 0 }}
+    animate={{ opacity: 1 }}
+    exit={{ opacity: 0 }}
+    transition={{ duration: 0.8 }}
+  >
+    <section className="overflow-x-hidden">
+      <PageUnderConstruction openModal={openConsultationModal}/>
+    </section>
+    {isConsultationOpen && (
+      <RequestConsultation
+        isOpen={isConsultationOpen}
+        onClose={closeConsultationModal}
+        redirectToContactForm={redirectToContactForm}
+      />
+    )}
+    {isContactOpen && (
+      <ConsultationForm isOpen={isContactOpen} onClose={closeContactModal} />
+    )}
+  </motion.section>
   );
 }
  

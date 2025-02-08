29c20e0c2120d11417fe0426a3a2f85728c86cf0
Prospertech-github <ikechukwuprosper8@gmail.com>
2025-02-08 08:57:41 +0100

    Fixing issues on merge conflict

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -99,6 +99,200 @@ import CardHouse from "./card-house.svg";
 import CardFace from "./card-face.svg";
 import ScheduleMeetingImg from "./schedule-meeting.webp";
 import ServicesHeroImg from "./services-hero.webp";
+
+export {
+  MainLogoSVG,
+  FooterLogoSVG, 
+  SkylineImg,
+  MissionImg,
+  PurposeImg,
+  StoryImg,
+  TeamImg,
+  VerifiedIcon,
+  MoneyHandIcon,
+  SmileyIcon,
+  RecycleIcon,
+  CreditCardIcon,
+  PeopleIcon,
+  BankIcon,
+  GlobeIcon,
+  CaseStudyImg1,
+  CaseStudyImg2,
+  CaseStudyImg3,
+  ArticleImg1,
+  ResourcesPageHeroImg,
+  CenBankLogo,
+  EcobankLogo,
+  GalaxyBackboneLogo,
+  KeystoneBankLogo,
+  MTNLogo,
+  PremiumPensionLogo,
+  StandardCharteredLogo,
+  SterlingBankLogo,
+  BankOfAmericaLogo,
+  BankOfBotswana,
+  FidelityBankLogo,
+  GTCOLogo,
+  FIRSLogo,
+  EtranzactLogo,
+  ShellLogo,
+  AXAInsurance,
+  ServicesImg1,
+  ServicesImg2,
+  ServicesImg3,
+  TrainingImg1,
+  TrainingImg2,
+  TrainingImg3,
+  ProtectionImg,
+  ReliabilityImg,
+  HomeHeroImage,
+  IsacaLogo,
+  PecbLogo,
+  SwiftLogo,
+  PciLogo,
+  ApmgLogo,
+  NistLogo,
+  OverviewImg1,
+  OverviewImg2,
+  OverviewImg3,
+  DigitalTrustImg,
+  DigitalTransformationImg,
+  DigitalSkillsDevImg,
+  SkillsDevImg1,
+  SkillsDevImg2,
+  SkillsDevImg3,
+  FormCover,
+  HomePageServicesImg,
+  HomePageCaseStudiesImg,
+  MotionMacbook,
+  MotionBulb,
+  MotionCoins,
+  MotionGraduation,
+  MotionGrowth,
+  MotionBrain,
+  HomeServicesIconBg,
+  OfficeLocationImg,
+  ErrorImg,
+  FixMain,
+  SkylineWhite,
+  NeuroscienceImg,
+  CompanyProfileImg,
+  HomeHeroImg,
+  ForwardImg,
+  HandShakeImg,
+  AboutUsHeroImg,
+  AboutUsMissionImg,
+  AboutUsPurposeImg,
+  AboutUsTeamImg,
+  AboutUsDownloadProfile,
+  AboutUsLinkBg,
+  ResourcesHero,
+  ProfileCardBg,
+  CaseStudiesBg,
+  ForwardImgMbl,
+  AboutUsLinkBgMbl,
+  HandShakeImgMbl,
+  AboutUsDownloadProfileMbl,
+  CardMoney,
+  CardChart,
+  CardAcceleration,
+  CardRoute,
+  CardHouse,
+  CardFace,
+  ScheduleMeetingImg,
+  ServicesHeroImg
+};import MainLogoSVG from "./main-logo.svg";
+import FooterLogoSVG from "./footer-logo.svg";
+import MoneyHandIcon from "./money-hand-icon.svg";
+import VerifiedIcon from "./verified-icon.svg";
+import RecycleIcon from "./recycle-icon.svg";
+import SmileyIcon from "./smiley-icon.svg";
+import CreditCardIcon from "./credit-card-icon.svg";
+import PeopleIcon from "./people-icon.svg";
+import BankIcon from "./bank-icon.svg";
+import GlobeIcon from "./globe-icon.svg";
+import SkylineImg from "./skyline.png";
+import MissionImg from "./mission-img.png";
+import PurposeImg from "./purpose-img.png";
+import StoryImg from "./story-img.png";
+import TeamImg from "./team-img.png";
+import CaseStudyImg1 from "./case-study-1.webp";
+import CaseStudyImg2 from "./case-study-2.webp";
+import CaseStudyImg3 from "./case-study-3.webp";
+import ArticleImg1 from "./article-1.webp";
+import ResourcesPageHeroImg from "./resource-page-hero-img.png";
+import CenBankLogo from "./cen-bank-logo.svg";
+import EcobankLogo from "./ecobank-logo.svg";
+import GalaxyBackboneLogo from "./galaxy-backbone-logo.svg";
+import KeystoneBankLogo from "./keystone-bank-logo.svg";
+import MTNLogo from "./mtn-logo.svg";
+import PremiumPensionLogo from "./premium-pension-logo.svg";
+import StandardCharteredLogo from "./standard-chartered-logo.svg";
+import BankOfAmericaLogo from "./boa-logo.svg";
+import BankOfBotswana from "./bank-of-botswana-logo.svg";
+import FidelityBankLogo from "./fidelity-logo.svg";
+import GTCOLogo from "./gtco-logo.svg";
+import AXAInsurance from "./axa-ins-logo.svg";
+import SterlingBankLogo from "./sterling-bank-logo.svg";
+import FIRSLogo from "./firs-logo.svg";
+import EtranzactLogo from "./e-tranzact-logo.svg";
+import ShellLogo from "./shell-logo.svg";
+import ServicesImg1 from "./services-1.webp";
+import ServicesImg2 from "./services-2.webp";
+import ServicesImg3 from "./services-3.webp";
+import TrainingImg1 from "./training-1.webp";
+import TrainingImg2 from "./training-2.webp";
+import TrainingImg3 from "./training-3.webp";
+import OverviewImg1 from "./overview-1.webp";
+import OverviewImg2 from "./overview-2.webp";
+import OverviewImg3 from "./overview-3.webp";
+import DigitalTrustImg from "./digital-trust-img.webp";
+import DigitalTransformationImg from "./digital-transformation.webp";
+import DigitalSkillsDevImg from "./digital-skills-dev.webp";
+import SkillsDevImg1 from "./skills-dev-img-1.png";
+import SkillsDevImg2 from "./skills-dev-img-2.png";
+import SkillsDevImg3 from "./skills-dev-img-3.png";
+import ProtectionImg from "./protection-img.webp";
+import ReliabilityImg from "./reliability.webp";
+import HomeHeroImage from "./home-hero.webp";
+import IsacaLogo from "./isaca-logo.svg";
+import PecbLogo from "./pecb-logo.svg";
+import SwiftLogo from "./swift-logo.svg";
+import PciLogo from "./pci-logo.svg";
+import ApmgLogo from "./apmg-logo.svg";
+import NistLogo from "./nist-logo.svg";
+import FormCover from "./form-cover.png";
+import HomePageServicesImg from "./home-page-services-icon.webp";
+import HomePageCaseStudiesImg from "./home-page-case-study-image.webp";
+import MotionMacbook from "./motion-macbook-icon.svg";
+import MotionBulb from "./motion-bulb-icon.svg";
+import MotionCoins from "./motion-coins-icon.svg";
+import MotionGraduation from "./motion-graduation-icon.svg";
+import MotionGrowth from "./motion-growth-icon.svg";
+import MotionBrain from "./motion-brain-icon.svg";
+import HomeServicesIconBg from "./home-services-icons-img.webp";
+import OfficeLocationImg from "./office-location.webp";
+import ErrorImg from "./error-image.webp";
+import FixMain from "./fix-main.webp";
+import SkylineWhite from "./skyline-white.webp";
+import NeuroscienceImg from "./neuroscience.webp";
+import CompanyProfileImg from "./company-profile.webp";
+
+import HomeHeroImg from "./home-hero-image.webp";
+import ForwardImg from "./forward-img.webp";
+import HandShakeImg from "./handshake-img.webp";
+import AboutUsHeroImg from "./about-us-hero.webp";
+import AboutUsMissionImg from "./mission-img.webp";
+import AboutUsPurposeImg from "./purpose-img.webp";
+import AboutUsTeamImg from "./team-img.webp";
+import AboutUsDownloadProfile from "./download-profile.webp";
+import AboutUsLinkBg from "./about-us-link-img.webp";
+import ResourcesHero from "./resourcesHero.webp";
+import ProfileCardBg from "./profileCardBg.webp";
+import CaseStudiesBg from "./caseStudiesBg.webp";
+import ForwardImgMbl from "./forward-img-mobile.webp";
+import AboutUsLinkBgMbl from "./about-us-link-mbl.webp";
+import HandShakeImgMbl from "./handshake-mbl-img.webp";
 import ProfileCardBgMbl from "./profileCardBgMbl.webp";
 
 export {
@@ -192,5 +386,6 @@ export {
   CaseStudiesBg,
   ForwardImgMbl,
   AboutUsLinkBgMbl,
-  HandShakeImgMbl
+  HandShakeImgMbl,
+  ProfileCardBgMbl
 };

9a6bf353769541c978e64c3b714f1cda68d41c55
Edozie Onyeanusi <phynnedo@gmail.com>
2025-05-23 09:53:59 +0100

    fix: made changes on case study card

--- src/pages/homePage/components/heroSection.tsx
+++ src/pages/homePage/components/heroSection.tsx
@@ -29,7 +29,7 @@ const HeroSection: React.FC<HeroSectionProps> = (props) => {
       <div className="flex justify-between items-center px-[5rem] 2xl:w-full mxxl:px-[3rem] mmlg:flex-col msm:px-4">
         <div className="w-1/2 2xl:w-[50%] mmlg:w-full mmd:pt-[3.5rem] msm:pt-[2.5rem] msm:w-full">
           <div className="w-[95%] msm:w-full">
-            <h1 className="text-[3rem] font-raleway text-white font-medium mb-6 mmlg:text-[40px] mmlg:text-left msm:text-[28px] mxxss:text-[24px]">
+            <h1 className="text-[3rem] font-raleway text-white font-medium mb-6 mmlg:text-[40px] mmlg:text-left msm:text-[28px] mxs:mb-2 mxxss:text-[24px]">
               Strengthening organizations,
               <br /> Equipping professionals.
             </h1>
@@ -46,13 +46,13 @@ const HeroSection: React.FC<HeroSectionProps> = (props) => {
             </Text>
             <div className="flex justify-start items-center gap-7 mmlg:flex-col mmlg:items-start mmlg:w-[45%] mmd:w-[65%] mxs:w-[75%]">
               <button
-                className="bg-afenoid-green/75 border-white border-[3px] py-[0.8rem] px-[2.5rem] flex justify-center items-center cursor-pointer text-white text-center text-base h-auto w-auto font-proxima-nova font-normal transition delay-150 duration-300 ease-in-out whitespace-nowrap hover:bg-afenoid-lemon/75 hover:border-transparent mxxs:text-sm"
+                className="bg-afenoid-green/75 border-white border-[3px] py-[0.8rem] px-[2.5rem] flex justify-center items-center cursor-pointer text-white text-center text-base h-auto w-auto font-proxima-nova font-normal transition delay-150 duration-300 ease-in-out whitespace-nowrap hover:bg-afenoid-lemon/75 hover:border-transparent mmlg:w-[80%] mxxs:text-sm"
                 onClick={openModal}
               >
                 Schedule a Meeting
               </button>
               <Link className="block w-full" to="/contact-us">
-                <button className="bg-transparent border-white border-[3px] py-[0.8rem] px-[2.5rem] flex justify-center items-center cursor-pointer text-white text-center text-base h-auto w-auto font-proxima-nova font-normal transition delay-150 duration-300 ease-in-out hover:bg-white hover:text-afenoid-lemon mxxs:text-sm">
+                <button className="bg-transparent border-white border-[3px] py-[0.8rem] px-[2.5rem] flex justify-center items-center cursor-pointer text-white text-center text-base h-auto w-auto font-proxima-nova font-normal transition delay-150 duration-300 ease-in-out whitespace-nowrap hover:bg-white hover:text-afenoid-lemon mmlg:w-[80%] mxxs:text-sm">
                   Contact Us
                 </button>
               </Link>
